d10dcfc51f00e1ec5ea51710003f8a7972c79506
SexualWhiteChocolate <karimullin.tr@phystech.edu>
2023-12-16 17:33:05 +0300

    Final commit?

--- task1/init.sh
+++ task1/init.sh
@@ -2,8 +2,7 @@
 sudo docker rm $(docker ps -a -q)
 sudo docker build -t karimullin_task1 .
 
-#docker tag karimullin_task1:v1.0 sexualwhitechocolate/karimullin_task1:v1.0
-#docker login
-#docker push sexualwhitechocolate/karimullin_task1:v1.0
+docker login
+docker push sexualwhitechocolate/karimullin_task1
 
 sudo docker run --rm -v karimullin_task1_volume:/ISD_BIT_Lab2 karimullin_task1 /bin/bash /ISD_BIT_Lab2/task1/run_from_docker.sh
--- task1/run_from_docker.sh
+++ task1/run_from_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd ISD_BIT_Lab2/task1/Consumer/
 nohup java -jar Consumer.jar &
+sleep 1
 cd ..
 cd Producer/
 nohup java -jar Producer.jar &
