ac22daab5749c596e5aa4bc584b44a4c91c8641b
SexualWhiteChocolate <karimullin.tr@phystech.edu>
2023-12-16 14:14:04 +0300

    Fourth commit

--- task1/run_from_docker.sh
+++ task1/run_from_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup java -jar IDS_BIT_Lab2/task1/Consumer.jar &
-nohup java -jar ISD_BIT_Lab2/task1/Producer.jar &
+nohup java -jar /ISD_BIT_Lab2/task1/Consumer/Consumer.jar &
+nohup java -jar /ISD_BIT_Lab2/task1/Producer/Producer.jar &
 sleep 20
 cat ISD_BIT_Lab2/task1/Consumer/db.txt
