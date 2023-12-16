eac256b47e54d05239d634a9c882ad8f3f466c07
SexualWhiteChocolate <karimullin.tr@phystech.edu>
2023-12-16 17:36:18 +0300

    task1 complete commit

--- task1/init.sh
+++ task1/init.sh
@@ -2,7 +2,8 @@
 sudo docker rm $(docker ps -a -q)
 sudo docker build -t karimullin_task1 .
 
-docker login
-docker push sexualwhitechocolate/karimullin_task1
+#docker tag karimullin_task1 sexualwhitechocolate/karimullin_task1
+#docker login
+#docker push sexualwhitechocolate/karimullin_task1
 
 sudo docker run --rm -v karimullin_task1_volume:/ISD_BIT_Lab2 karimullin_task1 /bin/bash /ISD_BIT_Lab2/task1/run_from_docker.sh
